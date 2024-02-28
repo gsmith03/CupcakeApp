618edb28b2521eb69e5c6ab5839222bc8ef3897d
Gwendolyn Smith-Hill <52078285+gsmith03@users.noreply.github.com>
2024-02-28 01:39:50 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -0,0 +1,17 @@
+//package com.example.cupcake.test
+//
+//import androidx.activity.ComponentActivity
+//import androidx.compose.ui.test.junit4.createAndroidComposeRule
+//import androidx.navigation.
+//
+//import org.junit.Rule
+//
+//
+//class CupcakeScreenNavigationTest {
+//
+//    @get:Rule
+//    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+//
+//    private lateinit var navController: TestNavHostController
+//
+//}
